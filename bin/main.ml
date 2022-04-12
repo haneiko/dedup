21a8ddb8615b8dc15abbb658b395f90dcf161678
Mateus C. Haneiko <mateus.haneiko@gmail.com>
2022-04-12 00:19:05 -0300

    list files and calc the md5 hash

--- bin/main.ml
+++ bin/main.ml
@@ -1,37 +1,40 @@
-type file = File of string | Dir of string * file list
-
-let rec list_files folder_name =
-  let file_list = ref [] in
-  try
-    let h = Unix.opendir folder_name in
-    (try
-       while true do
-         let entry2 = Unix.readdir h in
-         if entry2 <> "." && entry2 <> ".." then
-           let entry = folder_name ^ Filename.dir_sep ^ entry2 in
-           let stats = Unix.stat entry in
-           match stats.st_kind with
-           | Unix.S_DIR -> (
-               match list_files entry with
-               | Some sub_dir -> file_list := sub_dir :: !file_list
-               | None -> ())
-           | Unix.S_REG -> file_list := File entry :: !file_list
-           | _ -> ()
-       done
-     with End_of_file -> Unix.closedir h);
-    Some (Dir (folder_name, !file_list))
+let opendir path =
+  try Some (Unix.opendir path)
   with Unix.Unix_error (err, _, arg) ->
     Printf.printf "%s: %s\n" (Unix.error_message err) arg;
     None
 
-let rec print_files = function
-  | File name -> print_endline name
-  | Dir (name, file_list) ->
-      print_endline name;
-      ignore (List.map print_files file_list)
+let readdir dir_handle =
+  let content = ref [] in
+  try
+    while true do
+      match Unix.readdir dir_handle with
+      | "." | ".." -> ()
+      | entry -> content := entry :: !content
+    done;
+    None (* ? *)
+  with
+  | End_of_file ->
+      Unix.closedir dir_handle;
+      Some !content
+  | exn ->
+      Unix.closedir dir_handle;
+      raise exn
+
+let prepend_path p files = List.map (fun a -> p ^ Filename.dir_sep ^ a) files
+let is_directory p = try Sys.is_directory p with Sys_error _ -> false
+
+let rec list_files path =
+  match Option.bind (opendir path) readdir with
+  | None -> []
+  | Some content ->
+      let file_list = prepend_path path content in
+      let folders, files = List.partition is_directory file_list in
+      let subs = List.map list_files folders in
+      List.fold_left List.append files subs
 
 let () =
-  let folder_name = "." in
-  match list_files folder_name with
-  | Some files -> print_files files
-  | None -> ()
+  let files = list_files "." in
+  let prepend_hash a = Digest.to_hex (Digest.file a) ^ " " ^ a in
+  let files_with_hash = List.map prepend_hash files in
+  ignore (List.map print_endline files_with_hash)
