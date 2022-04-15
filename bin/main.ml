d26a9cc95d65252348c1952d56474d467263d65b
Mateus C. Haneiko <mateus.haneiko@gmail.com>
2022-04-14 22:12:19 -0300

    tmp file creation + EDITOR invocation

--- bin/main.ml
+++ bin/main.ml
@@ -36,22 +36,52 @@ let find_dups files =
   let hash_file a = Digest.to_hex (Digest.file a) in
   let hashes = List.map hash_file files in
   let tb = Hashtbl.create (List.length files) in
+  let fold k _ l =
+    match Hashtbl.find_all tb k with
+    | [] | [ _ ] | "" :: _ -> l
+    | dups ->
+        (* to avoid repeated keys: *)
+        Hashtbl.add tb k "";
+        dups :: l
+  in
   List.iter2 (fun f h -> Hashtbl.add tb h f) files hashes;
-  Hashtbl.fold
-    (fun k _ l ->
-      match Hashtbl.find_all tb k with
-      | [] | [ _ ] | "" :: _ -> l
-      | dups ->
-          (* to avoid repeated keys: *)
-          Hashtbl.add tb k "";
-          dups :: l)
-    tb []
+  Hashtbl.fold fold tb []
+
+let make_tmp_file text =
+  try
+    let file_name = Filename.temp_file "dedup_" "_files_to_remove" in
+    let file = Unix.openfile file_name [ Unix.O_CREAT; Unix.O_WRONLY ] 600 in
+    (try ignore (Unix.write_substring file text 0 (String.length text))
+     with Unix.Unix_error (err, _, arg) ->
+       Printf.printf "%s: %s\n" (Unix.error_message err) arg);
+    Unix.close file;
+    Some file_name
+  with Unix.Unix_error (err, _, arg) ->
+    Printf.printf "%s: %s\n" (Unix.error_message err) arg;
+    None
+
+let join sep list =
+  List.fold_left (fun a b -> if a != "" then a ^ sep ^ b else b) "" list
 
 let () =
-  let files = list_files "." in
-  let dups = find_dups files in
-  List.iter
-    (fun l ->
-      List.iter print_endline l;
-      print_endline "")
-    dups
+  match Sys.getenv_opt "EDITOR" with
+  | None -> print_endline "environment variable \"EDITOR\" not set"
+  | Some editor -> (
+      let files = list_files "." in
+      let dups = find_dups files in
+      let str =
+        "# Uncomment the files you want to remove\n"
+        ^ "# then save and quit the editor:\n\n"
+        ^ join "\n\n"
+            (List.map
+               (fun l -> List.map (fun a -> "#" ^ a) l |> join "\n")
+               dups)
+      in
+      match make_tmp_file str with
+      | None -> ()
+      | Some file_name -> (
+          match Unix.system (editor ^ " " ^ file_name) with
+          | Unix.WSIGNALED v -> Printf.printf "signaled %d\n" v
+          | Unix.WSTOPPED v -> Printf.printf "stopped %d\n" v
+          | Unix.WEXITED 0 -> ()
+          | Unix.WEXITED v -> Printf.printf "exited %d\n" v))
