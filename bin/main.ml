732f9c1610e3035b68e35b181c273e2ae69b9c45
Mateus C. Haneiko <mateus.haneiko@gmail.com>
2022-04-15 00:31:46 -0300

    let* in list_files

--- bin/main.ml
+++ bin/main.ml
@@ -24,13 +24,13 @@ let prepend_path p files = List.map (fun a -> p ^ Filename.dir_sep ^ a) files
 let is_directory p = try Sys.is_directory p with Sys_error _ -> false
 
 let rec list_files path =
-  match Option.bind (opendir path) readdir with
-  | None -> []
-  | Some content ->
-      let file_list = prepend_path path content in
-      let folders, files = List.partition is_directory file_list in
-      let subs = List.map list_files folders in
-      List.fold_left List.append files subs
+  let ( let* ) o f = match o with None -> [] | Some x -> f x in
+  let* dir = opendir path in
+  let* content = readdir dir in
+  let file_list = prepend_path path content in
+  let folders, files = List.partition is_directory file_list in
+  let subs = List.map list_files folders in
+  List.fold_left List.append files subs
 
 let find_dups files =
   let hash_file a = Digest.to_hex (Digest.file a) in
